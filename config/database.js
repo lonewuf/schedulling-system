e6983d315e65d9bd98fe55e4b1146f317010c0f5
isabuhay013 <john.nico.austria@gmail.com>
2019-09-23 12:47:50 -0700

    change db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
   databaseDev: 'mongodb://localhost/dentalclinic-dev',
-  daatabaseProd: 'mongodb://nico:niconicon1@ds039431.mlab.com:39431/travelgo'
+  daatabaseProd: 'mongodb://nico:n1con1@ds147003.mlab.com:47003/dental-clinic'
 }
