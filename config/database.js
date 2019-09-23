3a5898197114287179bf8d2993b8577b4ebc78a8
isabuhay013 <john.nico.austria@gmail.com>
2019-09-23 12:36:17 -0700

    add creds in db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
   databaseDev: 'mongodb://localhost/dentalclinic-dev',
-  daatabaseProd: 'mongodb://isabuhay013:l0n3wolf@ds039431.mlab.com:39431/travelgo'
+  daatabaseProd: 'mongodb://nico:niconicon1@ds039431.mlab.com:39431/travelgo'
 }
